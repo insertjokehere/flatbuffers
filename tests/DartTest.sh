563c742663450986651e1babacd8ede3222ed759
Will Hughes <william.hughes@taskaprosthetics.com>
2024-08-20 09:56:21 +1200

    Export all imported files in the same namespace to support recursive imports

--- tests/DartTest.sh
+++ tests/DartTest.sh
@@ -31,6 +31,7 @@ cd ../dart
 ../flatc --dart --gen-object-api -o ./test ./test/list_of_enums.fbs
 ../flatc --dart --gen-object-api -o ./test ./test/bool_structs.fbs
 ../flatc --dart --gen-object-api -o ./test ./test/enum_names.fbs
+../flatc --dart --gen-object-api -o ./test ./test/includes_*.fbs
 
 # update packages
 dart pub get
